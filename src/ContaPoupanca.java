af25418933b21f5773a40edd453a83cbee90d2f3
Luiz fernando <luizfernandodemoura.2013@gmail.com>
2024-02-20 14:14:00 -0300

    Class Completed

--- src/ContaPoupanca.java
+++ src/ContaPoupanca.java
@@ -1,4 +1,30 @@
-public class ContaPoupanca {
+import java.math.BigDecimal;
 
-    
+public class ContaPoupanca extends Conta{
+
+    private static final BigDecimal Rendimentos = BigDecimal.valueOf(0.02);
+
+    public ContaPoupanca(Cliente cliente, String numeroConta, String agencia) {
+        super(cliente,numeroConta, agencia, TipoConta.POUPANCA);
+    }
+
+    @Override
+    public void depositar(BigDecimal valor) {
+        saldo = saldo.add(valor);
+        System.out.println("deposito de " + valor + "Realizado com sucesso" + saldo );
+    }
+
+    @Override
+    public void sacar(BigDecimal valor) {
+        saldo = saldo.subtract(valor);
+        System.out.println("saque de " + valor + " realizado com sucesso! " + saldo);
+
+    }
+
+    public void aplicarRendimento() {
+        BigDecimal redimento = saldo.multiply(Rendimentos);
+        saldo =saldo.add(redimento);
+        System.out.println("Rendiemnto aplicado, novo saldo : " + saldo );
+    }
+
 }
