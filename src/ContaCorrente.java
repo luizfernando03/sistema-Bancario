547eeb204a76a8b52c4b6aebbc91f2418902233e
Luiz fernando <luizfernandodemoura.2013@gmail.com>
2024-02-20 11:07:31 -0300

    Completing Class ContaCorrente

--- src/ContaCorrente.java
+++ src/ContaCorrente.java
@@ -9,6 +9,14 @@ public abstract class ContaCorrente extends Conta{
     public ContaCorrente(Cliente cliente, String numeroConta, String agencia) {
         super(cliente, numeroConta, agencia, TipoConta.CORRENTE);
     }
+
+    public void sacar(BigDecimal valor){
+        BigDecimal taxaSaque = cliente.getTipo().equals("PF") ? TAXA_SAQUE_PF : TAXA_SAQUE_PJ;
+        BigDecimal valorConTaxa = valor.multiply(BigDecimal.ONE.subtract(taxaSaque));
+        saldo=saldo.subtract(valorConTaxa);
+        System.out.println("saque de : " + valor + "realizaado com sucesso . Novosaldo: " + saldo);
+
+    }
     @Override
     public void depositar(BigDecimal valor){
         saldo = saldo.add(valor);
