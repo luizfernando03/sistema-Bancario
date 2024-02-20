e1f3f701db8de0e568aff7201c67469f5dc152c1
Luiz fernando <luizfernandodemoura.2013@gmail.com>
2024-02-20 00:07:46 -0300

    Update client class

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,2 +1,53 @@
+import conta.Conta;
+
+import java.util.List;
+
 public class Cliente {
+
+    private String nome;
+    private String cpf;
+    private String endereco;
+    private String email;
+    private String tipo;
+    private String dataNascimento;
+    private List<Conta> contas;
+
+    public Cliente(String nome, String cpf, String endereco, String email, String tipo, String dataNascimento, List<Conta> contas) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.endereco = endereco;
+        this.email = email;
+        this.tipo = tipo;
+        this.dataNascimento = dataNascimento;
+        this.contas = contas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public List<Conta> getContas() {
+        return contas;
+    }
+    
 }
--- src/TipoConta.java
+++ src/TipoConta.java
@@ -1,2 +1,3 @@
 public enum TipoConta {
+    CORRENTE, POUPANCA, INVESTIMENTO
 }
