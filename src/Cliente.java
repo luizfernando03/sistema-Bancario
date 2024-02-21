9a3386004f16d204ab174c4e4ff3a986e6f9bd2a
Luiz Fernando <luizfernandodemoura.2013@gmail.com>
2024-02-21 01:03:47 -0300

    making correction

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,31 +1,31 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Cliente {
 
     private String nome;
-    private String cpf;
+    private String cpfCnpj;
     private String endereco;
     private String email;
     private String tipo;
-    private String dataNascimento;
+
     private List<Conta> contas;
 
-    public Cliente(String nome, String cpf, String endereco, String email, String tipo, String dataNascimento, List<Conta> contas) {
+    public Cliente(String nome, String documento, String endereco, String email, String tipo, List<Conta> contas) {
         this.nome = nome;
-        this.cpf = cpf;
+        this.cpfCnpj = documento;
         this.endereco = endereco;
         this.email = email;
         this.tipo = tipo;
-        this.dataNascimento = dataNascimento;
-        this.contas = contas;
+        this.contas = new ArrayList<>();
     }
 
     public String getNome() {
         return nome;
     }
 
-    public String getCpf() {
-        return cpf;
+    public String getCpfCnpj() {
+        return cpfCnpj;
     }
 
     public String getEndereco() {
@@ -40,12 +40,17 @@ public class Cliente {
         return tipo;
     }
 
-    public String getDataNascimento() {
-        return dataNascimento;
-    }
-
     public List<Conta> getContas() {
         return contas;
     }
 
+    // Método para adicionar uma conta ao cliente
+    public void adicionarConta(Conta conta) {
+        contas.add(conta);
+    }
+
+    // Método para remover uma conta do cliente
+    public void removerConta(Conta conta) {
+        contas.remove(conta);
+    }
 }
