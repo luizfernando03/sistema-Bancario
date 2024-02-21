d6ef5eb1de15f2889fae5723cab55b44b44403b7
Luiz Fernando <luizfernandodemoura.2013@gmail.com>
2024-02-21 01:20:39 -0300

    updating savings class

--- src/ContaCorrente.java
+++ src/ContaCorrente.java
@@ -2,24 +2,26 @@
 import java.math.BigDecimal;
 
 public abstract class ContaCorrente extends Conta{
-    private  static final BigDecimal TAXA_SAQUE_PF = BigDecimal.valueOf(0.02);
-    private  static final BigDecimal TAXA_SAQUE_PJ = BigDecimal.valueOf(0.005);
+    private static final BigDecimal TAXA_SAQUE = new BigDecimal("0.005"); // Taxa de 0.5% para saques
 
 
-    public ContaCorrente(Cliente cliente, String numeroConta, String agencia) {
-        super(cliente, numeroConta, agencia, TipoConta.CORRENTE);
+    // Construtor
+    public ContaCorrente(Cliente cliente, String numero, String agencia) {
+        super(cliente, numero, agencia);
     }
 
-    public void sacar(BigDecimal valor){
-        BigDecimal taxaSaque = cliente.getTipo().equals("PF") ? TAXA_SAQUE_PF : TAXA_SAQUE_PJ;
-        BigDecimal valorConTaxa = valor.multiply(BigDecimal.ONE.subtract(taxaSaque));
-        saldo=saldo.subtract(valorConTaxa);
-        System.out.println("saque de : " + valor + "realizaado com sucesso . Novosaldo: " + saldo);
-
-    }
+    // Implementação do método depositar
     @Override
-    public void depositar(BigDecimal valor){
+    public void depositar(BigDecimal valor) {
         saldo = saldo.add(valor);
+    }
+
+
+    // Implementação do método sacar
+    @Override
+    public void sacar(BigDecimal valor) {
+        BigDecimal taxa = valor.multiply(TAXA_SAQUE);
+        saldo = saldo.subtract(valor.add(taxa));
     }
 
 }
