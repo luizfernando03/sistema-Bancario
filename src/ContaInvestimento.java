dab8629a14764f1700dfb8d5061d7fe1d88a019f
Luiz Fernando <luizfernandodemoura.2013@gmail.com>
2024-02-21 01:24:59 -0300

    logic of the ContaInvestimento class

--- src/ContaInvestimento.java
+++ src/ContaInvestimento.java
@@ -1,2 +1,29 @@
-public class ContaInvestimento {
+import java.math.BigDecimal;
+
+public class ContaInvestimento extends Conta{
+
+    private static final BigDecimal RENDIMENTO_EXTRA = new BigDecimal("0.02"); // Rendimento extra de 2%
+
+    // Construtor
+    public ContaInvestimento(Cliente cliente, String numero, String agencia) {
+        super(cliente, numero, agencia);
+    }
+
+    // Implementação do método depositar
+    @Override
+    public void depositar(BigDecimal valor) {
+        saldo = saldo.add(valor);
+    }
+
+    // Implementação do método sacar
+    @Override
+    public void sacar(BigDecimal valor) {
+        saldo = saldo.subtract(valor);
+    }
+
+    // Método para calcular os rendimentos
+    public void calcularRendimentos() {
+        saldo = saldo.multiply(RENDIMENTO_EXTRA.add(BigDecimal.ONE));
+    }
+
 }
